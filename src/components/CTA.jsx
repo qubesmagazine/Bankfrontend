692b3bf9375e3d00ca4a854159283a1370ed5dd0
okwudili onyido <qubesentertainmentmagazine@yahoo.com>
2024-02-07 02:42:16 +0100

    New Bank

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,22 +1,64 @@
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import styles from "../style";
 import Button from "./Button";
 
-const CTA = () => (
-  <section
-    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
-  >
-    <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Let’s try our service now!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
-      </p>
-    </div>
+const CTA = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
 
-    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button />
-    </div>
-  </section>
-);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    // Clean up observer
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <section
+      ref={sectionRef}
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <motion.div
+        className="flex-1 flex flex-col"
+        initial={{ opacity: 0, y: -50 }}
+        animate={isVisible ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.5 }}
+      >
+        <h2 className={styles.heading2}>Let’s try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </motion.div>
+
+      <motion.div
+        className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}
+        initial={{ opacity: 0, y: -50 }}
+        animate={isVisible ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.5 }}
+      >
+        <Button />
+      </motion.div>
+    </section>
+  );
+};
 
 export default CTA;
